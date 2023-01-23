7f20b20671b55b7791ffc3c7b777ffa618f845bb
svvimming <benmunozj@gmail.com>
2023-01-23 13:37:03 -0500

    feat: add multiple storage providers to CID tabbed slider

--- packages/fe/components/cid-card.vue
+++ packages/fe/components/cid-card.vue
@@ -3,111 +3,115 @@
     <div class="cid-card">
 
       <div class="panel-top">
+        <div class="top-content">
 
-        <div class="cid-title">
-          <div class="title">
-            {{ title }}
-          </div>
-          <div class="hash">
-            {{ hash }}
-          </div>
-        </div>
-
-        <div class="filetypes">
-          <div
-            v-for="filetype in filetypes"
-            :key="filetype"
-            class="file-extension">
-            {{ filetype }}
-          </div>
-        </div>
-
-        <div class="size">
-          {{ size }}
-        </div>
-
-        <div class="replicas">
-          <div class="replica-list">
-            <div
-              v-for="n in 5"
-              :key="`replica-${n}`"
-              :class="['replica', { exists: storageProviders[n - 1] }]">
+          <div class="cid-title">
+            <div class="title">
+              {{ title }}
+            </div>
+            <div class="hash">
+              {{ hash }}
             </div>
           </div>
-          <span>
-            {{ `x${storageProviders.length} Replicas` }}
-          </span>
-        </div>
 
-        <div class="expiry-date">
-          <span>{{ expiryDate }}</span>
-          <span>Avail. Until</span>
-        </div>
+          <div class="filetypes">
+            <div
+              v-for="filetype in filetypes"
+              :key="filetype"
+              class="file-extension">
+              {{ filetype }}
+            </div>
+          </div>
 
-        <div
-          :class="['status', status]">
-          {{ status.charAt(0).toUpperCase() + status.slice(1) }}
-        </div>
+          <div class="size">
+            {{ size }}
+          </div>
 
-        <div class="accordion-toggle">
-          <ButtonToggle
-            theme="light"
-            :class="[{ active: open }]"
-            @click="toggleBottomPanel">
-            {{ open ? 'Less' : 'More' }}
-          </ButtonToggle>
-        </div>
+          <div class="replicas">
+            <div class="replica-list">
+              <div
+                v-for="n in 5"
+                :key="`replica-${n}`"
+                :class="['replica', { exists: storageProviders[n - 1] }]">
+              </div>
+            </div>
+            <span>
+              {{ `x${storageProviders.length} Replicas` }}
+            </span>
+          </div>
 
+          <div class="expiry-date">
+            <span>{{ expiryDate }}</span>
+            <span>Avail. Until</span>
+          </div>
+
+          <div
+            :class="['status', status]">
+            {{ status.charAt(0).toUpperCase() + status.slice(1) }}
+          </div>
+
+          <div class="accordion-toggle">
+            <ButtonToggle
+              theme="light"
+              :class="[{ active: open }]"
+              @click="toggleBottomPanel">
+              {{ open ? 'Less' : 'More' }}
+            </ButtonToggle>
+          </div>
+
+        </div>
       </div>
 
       <div :class="['panel-bottom', { open }]">
-        <TabbedSlider
-          slider-id="cid-card-slider">
+        <div class="bottom-content">
+          <TabbedSlider
+            slider-id="cid-card-slider">
 
-          <template #before-tabs>
-            Select a storage provider to view retrieval commands
-          </template>
+            <template #before-tabs>
+              Select a storage provider to view retrieval commands
+            </template>
 
-          <template
-            v-for="(tab, i) in storageProviders"
-            #[`slider-tab-${i}`]>
-            <div
-              :key="`slider-tab-${i}`"
-              class="tab-row">
-              <div>
-                {{ tab.id }}
-              </div>
-              <div>
-                {{ tab.dealId }}
-              </div>
-              <div>
-                {{ tab.expiry_date }}
-              </div>
-              <div>
-                {{ tab.retrieval_rate }}
-              </div>
-            </div>
-          </template>
-
-          <template #before-slides>
-            Retrieval Commands
-          </template>
-
-          <template
-            v-for="(slide, i) in storageProviders"
-            #[`slider-panel-${i}`]>
-            <div
-              :key="`slider-panel-${i}`"
-              class="command-wrapper">
+            <template
+              v-for="(tab, i) in storageProviders"
+              #[`slider-tab-${i}`]>
               <div
-                v-for="(command, j) in slide.retrieval_commands"
-                :key="`command-${j}`">
-                {{ command }}
+                :key="`slider-tab-${i}`"
+                class="tab-row">
+                <div>
+                  {{ tab.id }}
+                </div>
+                <div>
+                  {{ tab.dealId }}
+                </div>
+                <div>
+                  {{ tab.expiry_date }}
+                </div>
+                <div>
+                  {{ tab.retrieval_rate }}
+                </div>
               </div>
-            </div>
-          </template>
+            </template>
 
-        </TabbedSlider>
+            <template #before-slides>
+              Retrieval Commands
+            </template>
+
+            <template
+              v-for="(slide, i) in storageProviders"
+              #[`slider-panel-${i}`]>
+              <div
+                :key="`slider-panel-${i}`"
+                class="command-wrapper">
+                <div
+                  v-for="(command, j) in slide.retrieval_commands"
+                  :key="`command-${j}`">
+                  {{ command }}
+                </div>
+              </div>
+            </template>
+
+          </TabbedSlider>
+        </div>
       </div>
 
     </div>
@@ -179,10 +183,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// /////////////////////////////////////////////////////////////////// Panel Top
+// ///////////////////////////////////////////////////////////////////// General
 .panel-top {
+  border-bottom: solid 1px black;
+}
+
+.panel-bottom {
+  height: 0;
+  overflow: hidden;
+  &.open {
+    height: auto;
+  }
+}
+
+.top-content,
+.bottom-content {
+  padding: 2.5rem 1.875rem;
+}
+
+.top-content {
   display: flex;
 }
+
+// //////////////////////////////////////////////////////////////////// CID INFO
 
 .filetypes,
 .replica-list {
@@ -220,12 +243,11 @@ export default {
   }
 }
 
-// //////////////////////////////////////////////////////////////// Panel Bottom
-.panel-bottom {
-  height: 0;
-  overflow: hidden;
-  &.open {
-    height: auto;
-  }
+// ///////////////////////////////////////////////////////////////// SLIDER TABS
+.tab-row {
+  display: flex;
 }
+
+// /////////////////////////////////////////////////////////////// SLIDER SLIDES
+
 </style>
--- packages/fe/components/cid-table.vue
+++ packages/fe/components/cid-table.vue
@@ -49,6 +49,36 @@ export default {
                 'lotus client retrieve --miner f022352 baga6ea4seaqih7gfqvkpvez3qlbajjolszk7vfqaatt5m2rdef52p3fq4jfpigi publicdomainmovies.tar.01',
                 'pow ffs get baga6ea4seaqih7gfqvkpvez3qlbajjolszk7vfqaatt5m2rdef52p3fq4jfpigi retrieval.png'
               ]
+            },
+            {
+              id: 'F0123123',
+              dealId: '1438903',
+              expiry_date: 'Feb 28 2023',
+              retrieval_rate: '100%',
+              retrieval_commands: [
+                'lotus client retrieve --miner f022352 b356c864441e1ca71dd4fe1fc455862350f6798c1f966a90ea3fcf60713b3548 publicdomainmovies.tar.01',
+                'pow ffs get b356c864441e1ca71dd4fe1fc455862350f6798c1f966a90ea3fcf60713b3548 retrieval.png'
+              ]
+            },
+            {
+              id: 'F012H890',
+              dealId: '1472892',
+              expiry_date: 'Mar 28 2023',
+              retrieval_rate: '100%',
+              retrieval_commands: [
+                'lotus client retrieve --miner f022352 88a601f0915f70051a32ac1c1e4b931c348467e70a0c2f7f72086967eeb79e29 publicdomainmovies.tar.01',
+                'pow ffs get 88a601f0915f70051a32ac1c1e4b931c348467e70a0c2f7f72086967eeb79e29 retrieval.png'
+              ]
+            },
+            {
+              id: 'F0126789',
+              dealId: '1472890',
+              expiry_date: 'Apr 1 2023',
+              retrieval_rate: '100%',
+              retrieval_commands: [
+                'lotus client retrieve --miner f022352 444d11afee588638e0479240c55abc5dc8c21ef829f519b8a96db9134b6b41f0 publicdomainmovies.tar.01',
+                'pow ffs get 444d11afee588638e0479240c55abc5dc8c21ef829f519b8a96db9134b6b41f0 retrieval.png'
+              ]
             }
           ],
           status: 'active'
