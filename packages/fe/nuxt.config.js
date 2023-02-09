9e31d57d8ad4e553e5e5aae74cc41f5df88bde67
Nauras Jabari <nauras@agencyundone.com>
2023-02-09 15:45:15 -0500

    fix: filters module store now wraps state output in a function to avoid SSR cross-polluting of data

--- packages/fe/nuxt.config.js
+++ packages/fe/nuxt.config.js
@@ -46,7 +46,7 @@ export default {
   },
   render: {
     bundleRenderer: {
-      runInNewContext: process.env.NODE_ENV === 'development' ? true : 'once' // https://v2.ssr.vuejs.org/api/#runinnewcontext
+      runInNewContext: false
     }
   },
   // /////////////////////////////////////////////////////// Headers of the Page
